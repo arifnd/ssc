5bbdc2b1cfc8af22ede7ce09e4b5c63e98eab3c7
Ari Effendi <arieffendi@gmail.com>
2020-03-13 20:38:56 +0700

    - make quiet install - check while file exist - fix failed on extract

--- install-nginx-webmin-module.sh
+++ install-nginx-webmin-module.sh
@@ -2,11 +2,25 @@
 
 # must root
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
+    echo "This script must be run as root" 
+    exit 1
 fi
 
-apt install nginx libhtml-parser-perl -y
-wget https://www.justindhoffman.com/sites/justindhoffman.com/files/nginx-0.11.wbm_.gz
-tar -xf nginx-0.11.wbm_.gz
+echo -ne "Installing depedency..."
+apt install nginx libhtml-parser-perl -yqq
+echo "[DONE]"
+
+# if file not exist
+if [[ ! -f "nginx-0.11.wbm_" ]]; then
+    echo -ne "Downloading..."
+    wget -q https://www.justindhoffman.com/sites/justindhoffman.com/files/nginx-0.11.wbm_.gz
+    echo "[DONE]"
+
+    echo -ne "Extracting..."
+    gunzip nginx-0.11.wbm_.gz
+    echo "[DONE]"
+fi
+
+echo -ne "Installing..."
 /usr/share/webmin/install-module.pl nginx-0.11.wbm_
+echo "[DONE]"
