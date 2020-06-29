806f7318d47b6581b79d33feb6c7cd68253fed2b
Ari Effendi <arieffendi@gmail.com>
2020-06-30 00:52:41 +0700

    first init

---
+++ install-docker-ubuntu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# must root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+apt-get update
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io -y
